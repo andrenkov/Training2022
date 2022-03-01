164db88332cb0500505b5c5d70bba8aab7656934
Vladimir Andrenkov <andrenkov@gmail.com>
2022-02-28 19:32:26 -0500

    test unit added

---
+++ dotNet_6/CalcLibUnitTest/CalcUnitTests.cs
@@ -0,0 +1,41 @@
+using Chapter4_TestUnit;
+using Xunit;
+
+namespace CalcLibUnitTest
+{
+    public class CalcUnitTests
+    {
+        [Fact]
+        public void TestAdding2and2()
+        {
+            //arrange
+            double a = 2;
+            double b = 2;
+            double expected = 4;
+            Calculator calc = new Calculator();
+
+            //act
+            double actual = calc.AddDouble(a, b);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAdding2and3()
+        {
+            //arrange
+            double a = 2;
+            double b = 3;
+            double expected = 5;
+            Calculator calc = new Calculator();
+
+            //act
+            double actual = calc.AddDouble(a, b);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ dotNet_6/Chapter4_TestUnit/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Chapter4_TestUnit
+{
+    public class Calculator
+    {
+        public double AddDouble(double a, double b)
+        { 
+            return a + b; 
+        }
+    }
+}
