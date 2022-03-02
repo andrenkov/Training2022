ca3f2b54a46180a6659535424b09fe63db1564c5
Vladimir Andrenkov <andrenkov@gmail.com>
2022-03-02 15:10:07 -0500

    added

---
+++ dotNet_6/Chapter 06/PacktLib/PacktLib/Person.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+namespace PacktLib
+{
+    public class Person : Object
+    {
+        //fields
+        public string? Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new();
+        //methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:ddd}.");
+        }
+
+        //Add statis method
+        public static Person Procreate(Person p1, Person p2)
+        {
+            Person baby = new() { 
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+
+            return baby;
+        }
+
+        //Add instance method
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+
+
+    }
+}
---
+++ dotNet_6/Chapter 06/PeopleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using PacktLib;
+
+Person harry = new() { Name = "Harry" };
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+
+//call instance method
+Person baby1 = mary.ProcreateWith(harry);
+baby1.Name = "Gary";
+
+//vcall statis method
+Person baby2 = Person.Procreate(harry, jill);
+
+WriteLine($"{harry.Name} has {harry.Children.Count} children");
+WriteLine($"{mary.Name} has {mary.Children.Count} children");
+WriteLine($"{jill.Name} has {jill.Children.Count} children");
+
+WriteLine(format:"{0}'s first child is named \"{1}\" ", arg0:harry.Name, arg1: harry.Children[0].Name);
+
